516138e2ebbaf08c1edb5c9177bb96d9ba893118
dramekanjie <dramekanjii@gmail.com>
2023-09-17 17:16:44 -0400

    changes made to Home page, Nav Bar and Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+
+import "../styles/Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <div className="socialMedia"></div>
+      <div className="socialMedia">
+        <InstagramIcon />
+        <FacebookIcon />
+      </div>
       <p>&copy; 2023 NourLight</p>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import HomeImage from "../assets/fonio.jpeg";
+import HomeImage from "../assets/jollof-bg.png";
 import "../styles/Home.css";
 
 function Home() {
+  const backgroundImageStyle = {
+    backgroundImage: `url(${HomeImage})`,
+    backgroundSize: "60%", // Adjust the background size as needed (e.g., 60%)
+    backgroundPosition: "center", // Center the background image
+  };
+
   return (
-    <div className="home" style={{ backgroundImage: `url(${HomeImage})` }}>
+    <div className="home" style={backgroundImageStyle}>
       <div className="headerContainer">
         <h1>NourLight</h1>
         <p>
-          Retrouvez de la lumière dans vos assiètes à travers nos mets
+          Retrouvez de la lumière dans vos assiettes à travers nos mets
           Africains.
         </p>
         <Link to="/menu">
