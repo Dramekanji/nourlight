dc09196645c2ff1449270acaa613a54a258247f5
dramekanjie <dramekanjii@gmail.com>
2023-09-18 16:29:52 -0400

    adjusted components on the home page to  make them more proportionate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import HomeImage from "../assets/jollof-bg.png";
 import "../styles/Home.css";
+import Special1 from "../assets/special1.png"; // Make sure to provide the correct file extension
+import Special2 from "../assets/special2.png"; // Make sure to provide the correct file extension
+import Special3 from "../assets/special3.png";
 
 function Home() {
   const backgroundImageStyle = {
@@ -22,12 +25,17 @@ function Home() {
           <button>Commandez Ici</button>
         </Link>
       </div>
-      <div className="specialText"></div>
       <div className="foodCardContainer">
         <h1>Nos Specialités</h1>
-        <div className="foodCard1"></div>
-        <div className="foodCard2"></div>
-        <div className="foodCard3"></div>
+        <div className="foodCard1">
+          <img src={Special1} alt="Special1" />
+        </div>
+        <div className="foodCard2">
+          <img src={Special2} alt="Special2" />
+        </div>
+        <div className="foodCard3">
+          <img src={Special3} alt="Special3" />
+        </div>
       </div>
     </div>
   );
