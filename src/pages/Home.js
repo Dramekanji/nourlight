23d5979cba9394cfca50fa634dc47081c95f393a
dramekanjie <dramekanjii@gmail.com>
2023-09-18 20:52:12 -0400

    created a function that allows uers to click on the cards and preview specialty dishes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import HomeImage from "../assets/jollof-bg.png";
 import "../styles/Home.css";
-import Special1 from "../assets/special1.png"; // Make sure to provide the correct file extension
-import Special2 from "../assets/special2.png"; // Make sure to provide the correct file extension
+import Special1 from "../assets/special1.png";
+import Special2 from "../assets/special2.png";
 import Special3 from "../assets/special3.png";
 
 function Home() {
+  const [backgroundImage, setBackgroundImage] = useState(HomeImage);
+
+  const handleCardClick = (newImage) => {
+    // Update the background image when a card is clicked
+    setBackgroundImage(newImage);
+  };
+
   const backgroundImageStyle = {
-    backgroundImage: `url(${HomeImage})`,
-    backgroundSize: "60%", // Adjust the background size as needed (e.g., 60%)
-    backgroundPosition: "center", // Center the background image
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "60%",
+    backgroundPosition: "center top",
+    transition: "background-image 0.3s ease",
   };
 
   return (
@@ -27,14 +35,17 @@ function Home() {
       </div>
       <div className="foodCardContainer">
         <h1>Nos Specialités</h1>
-        <div className="foodCard1">
+        <div className="foodCard1" onClick={() => handleCardClick(Special1)}>
           <img src={Special1} alt="Special1" />
+          <p>Special 1</p>
         </div>
-        <div className="foodCard2">
+        <div className="foodCard2" onClick={() => handleCardClick(Special2)}>
           <img src={Special2} alt="Special2" />
+          <p>Special 2</p>
         </div>
-        <div className="foodCard3">
+        <div className="foodCard3" onClick={() => handleCardClick(Special3)}>
           <img src={Special3} alt="Special3" />
+          <p>Special 3</p>
         </div>
       </div>
     </div>
